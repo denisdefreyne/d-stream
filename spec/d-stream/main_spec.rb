ae0c049977496bbfbb86a4602350be3de4279800
Denis Defreyne <denis.defreyne@stoneship.org>
2021-11-05 19:40:53 +0100

    Fix RSpec/NamedSubject offenses

--- spec/d-stream/main_spec.rb
+++ spec/d-stream/main_spec.rb
@@ -2,38 +2,38 @@
 
 describe DStream do
   describe '.map' do
-    subject { transformer.call([1, 2, 3]) }
+    subject(:result) { transformer.call([1, 2, 3]) }
 
     let(:transformer) { described_class.map { |e| e * 2 } }
 
     it { is_expected.to be_a(Enumerator) }
 
     example do
-      expect(subject.to_a).to eq([2, 4, 6])
+      expect(result.to_a).to eq([2, 4, 6])
     end
   end
 
   describe '.with_next' do
-    subject { transformer.call([1, 2, 3]) }
+    subject(:result) { transformer.call([1, 2, 3]) }
 
     let(:transformer) { described_class.with_next }
 
     it { is_expected.to be_a(Enumerator) }
 
     example do
-      expect(subject.to_a).to eq([[1, 2], [2, 3], [3, nil]])
+      expect(result.to_a).to eq([[1, 2], [2, 3], [3, nil]])
     end
   end
 
   describe '.chunk' do
-    subject { transformer.call([1, 1, 2, 3, 3, 3]) }
+    subject(:result) { transformer.call([1, 1, 2, 3, 3, 3]) }
 
     let(:transformer) { described_class.chunk(&:even?) }
 
     it { is_expected.to be_a(Enumerator) }
 
     example do
-      expect(subject.to_a).to eq(
+      expect(result.to_a).to eq(
         [
           [false, [1, 1]],
           [true, [2]],
